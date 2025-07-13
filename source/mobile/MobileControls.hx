ed7af263da4b61a6917aa4918098ecf029db5172
ryzen-x7 <azeitonax7@gmail.com>
2025-07-12 21:47:59 -0300

    Create MobileControls.hx

---
+++ source/mobile/MobileControls.hx
@@ -0,0 +1,37 @@
+import flixel.FlxG;
+import flixel.group.FlxGroup;
+import mobile.ui.VirtualButton;
+
+class MobileControls extends FlxGroup {
+
+public var left:VirtualButton; public var right:VirtualButton; public var up:VirtualButton; public var down:VirtualButton; public var a:VirtualButton; public var b:VirtualButton;
+
+public function new() {
+    super();
+
+    left = new VirtualButton(50, FlxG.height - 200, "left");
+    down = new VirtualButton(130, FlxG.height - 130, "down");
+    up = new VirtualButton(130, FlxG.height - 270, "up");
+    right = new VirtualButton(210, FlxG.height - 200, "right");
+
+    a = new VirtualButton(FlxG.width - 210, FlxG.height - 200, "a");
+    b = new VirtualButton(FlxG.width - 130, FlxG.height - 270, "b");
+
+    add(left);
+    add(down);
+    add(up);
+    add(right);
+    add(a);
+    add(b);
+}
+
+public function updateInput():Void {
+    if (left.isPressed()) FlxG.keys.pressed.LEFT = true;
+    if (right.isPressed()) FlxG.keys.pressed.RIGHT = true;
+    if (up.isPressed()) FlxG.keys.pressed.UP = true;
+    if (down.isPressed()) FlxG.keys.pressed.DOWN = true;
+    if (a.isPressed()) FlxG.keys.pressed.Z = true;
+    if (b.isPressed()) FlxG.keys.pressed.X = true;
+}
+
+}
